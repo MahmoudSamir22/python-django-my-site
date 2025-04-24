cf2b3d4628ad4cfb38dfca3089ed3f59c2f9f963
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-24 18:38:35 +0200

    handel model str return function

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,16 @@ from django.utils.text import slugify
 class Tag(models.Model):
     caption = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Post(models.Model):
     title = models.CharField(max_length= 80)
