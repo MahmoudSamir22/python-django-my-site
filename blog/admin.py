f562d124fe13473faa02379599c9c7a6bc04bb21
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-28 13:25:11 +0300

    Register Comment Model - Enhance View Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Author, Post, Tag
+from blog.models import Author, Post, Tag, Comment
 # Register your models here.
 
 class TagAdmin(admin.ModelAdmin):
@@ -12,6 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "excerpt", "image", "date", "slug", "content", "author",)
     prepopulated_fields = {"slug": ('title',)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_name", "user_email", "text")
+
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
